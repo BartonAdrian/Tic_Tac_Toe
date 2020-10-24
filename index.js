0a840e8fcaa81378795aad2eb495fc6b16bf6f6c
BartonAdrian <adrian.barton@tul.cz>
2020-10-24 21:46:35 +0200

    Vykreslovani

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//Game properties
+let natahu = 'circle';
+let hraje = document.querySelector('.hra');
+let tlacitko = document.querySelectorAll('button');
+
+for (let i = 0; i < tlacitko.length; i++) {
+  tlacitko[i].addEventListener('click', (event) => {
+
+    switch(natahu){
+      case 'circle' : 
+        event.target.classList.toggle('board_field--circle');
+        hraje.innerHTML = `hraje:
+        <img class="ikonka" src="obrazek/cross.svg" alt="křížek" />`;
+        tlacitko[i].style.backgroundImage = "url('obrazek/circle.svg')";
+        tlacitko[i].style.backgroundRepeat = "no-repeat";
+        tlacitko[i].style.backgroundSize = "90px 90px";
+        tlacitko[i].style.backgroundPosition = "center";
+        event.target.setAttribute('disabled', true);
+        natahu = 'cross';
+        break;
+      case 'cross':
+        event.target.classList.toggle('board_field--cross');
+        hraje.innerHTML = ` hraje:
+        <img class="ikonka" src="obrazek/circle.svg" alt="kolečko" />`;
+        tlacitko[i].style.backgroundImage = "url('obrazek/cross.svg')";
+        tlacitko[i].style.backgroundRepeat = "no-repeat";
+        tlacitko[i].style.backgroundSize = "90px 90px";
+        tlacitko[i].style.backgroundPosition = "center";
+        event.target.setAttribute('disabled', true);
+        natahu = 'circle'; 
+        break;
+    }
+  });
+}
