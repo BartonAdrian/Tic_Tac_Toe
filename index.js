da7e42d6faecdf093346dc089396b924017481b3
Adrian Bartoň <adrian.barton@tul.cz>
2020-10-25 08:54:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,32 +5,100 @@ let natahu = 'circle';
 let hraje = document.querySelector('.hra');
 let tlacitko = document.querySelectorAll('button');
 
-for (let i = 0; i < tlacitko.length; i++) {
-  tlacitko[i].addEventListener('click', (event) => {
+//Button size
+//const btn = tlacitko.style.width;
+const bingo = 3;
 
-    switch(natahu){
-      case 'circle' : 
-        event.target.classList.toggle('board_field--circle');
-        hraje.innerHTML = `hraje:
+for (let i = 0; i < tlacitko.length; i++) {
+    tlacitko[i].addEventListener('click', (event) => {
+
+        switch (natahu) {
+            case 'circle':
+                event.target.classList.toggle('board_field--circle');
+                hraje.innerHTML = `hraje:
         <img class="ikonka" src="obrazek/cross.svg" alt="křížek" />`;
-        tlacitko[i].style.backgroundImage = "url('obrazek/circle.svg')";
-        tlacitko[i].style.backgroundRepeat = "no-repeat";
-        tlacitko[i].style.backgroundSize = "90px 90px";
-        tlacitko[i].style.backgroundPosition = "center";
-        event.target.setAttribute('disabled', true);
-        natahu = 'cross';
-        break;
-      case 'cross':
-        event.target.classList.toggle('board_field--cross');
-        hraje.innerHTML = ` hraje:
+                tlacitko[i].style.backgroundImage = "url('obrazek/circle.svg')";
+                tlacitko[i].style.backgroundRepeat = "no-repeat";
+                tlacitko[i].style.backgroundSize = "40px 40px";
+                tlacitko[i].style.backgroundPosition = "center";
+                tlacitko[i].setAttribute("alt", "Cross");
+                event.target.setAttribute('disabled', true);
+                natahu = 'cross';
+                chceckWin();
+                break;
+            case 'cross':
+                event.target.classList.toggle('board_field--cross');
+                hraje.innerHTML = ` hraje:
         <img class="ikonka" src="obrazek/circle.svg" alt="kolečko" />`;
-        tlacitko[i].style.backgroundImage = "url('obrazek/cross.svg')";
-        tlacitko[i].style.backgroundRepeat = "no-repeat";
-        tlacitko[i].style.backgroundSize = "90px 90px";
-        tlacitko[i].style.backgroundPosition = "center";
-        event.target.setAttribute('disabled', true);
-        natahu = 'circle'; 
-        break;
+                tlacitko[i].style.backgroundImage = "url('obrazek/cross.svg')";
+                tlacitko[i].style.backgroundRepeat = "no-repeat";
+                tlacitko[i].style.backgroundSize = "40px 40px";
+                tlacitko[i].style.backgroundPosition = "center";
+                tlacitko[i].setAttribute("alt", "Circle");
+                event.target.setAttribute('disabled', true);
+                natahu = 'circle';
+                chceckWin();
+                break;
+        }
+    });
+}
+
+function chceckWin() {
+    for (let i = 0; i < tlacitko.length; i++) {
+        if (tlacitko[i].getAttribute("alt") !== null) {
+            rowWin(i);
+            columnWin(i);
+            awryWin(i);
+        }
     }
-  });
+}
+
+function rowWin(index) {
+    const znak = tlacitko[index].getAttribute("alt");
+    let pocet = 1;
+    for (let i = 1; i <= bingo; i++) {
+        if (znak === tlacitko[index + i].getAttribute("alt")) {
+            pocet++;
+        }
+    }
+    if (pocet === bingo) {
+        gameOver();
+    }
+}
+
+function columnWin(index) {
+    const znak = tlacitko[index].getAttribute("alt");
+    let pocet = 1;
+    for (let i = 1; i < bingo; i++) {
+        if (znak === tlacitko[index + (i * 10)].getAttribute("alt")) {
+            pocet++;
+        }
+    }
+    if (pocet === bingo) {
+        gameOver();
+    }
+}
+
+function awryWin(index) {
+    const znak = tlacitko[index].getAttribute("alt");
+    let pocet = 1;
+    //left-down
+    for (let i = 1; i < bingo; i++) {
+        if (znak === tlacitko[index + (i * 11)].getAttribute("alt")) {
+            pocet++;
+        }
+    }
+    //right-down
+    for (let i = 1; i < bingo; i++) {
+        if (znak === tlacitko[index + (i * 9)].getAttribute("alt")) {
+            pocet++;
+        }
+    }
+    if (pocet === bingo) {
+        gameOver();
+    }
+}
+
+function gameOver() {
+    alert("Konec hry");
 }
